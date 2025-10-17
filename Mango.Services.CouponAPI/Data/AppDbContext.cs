73198a6bf69d57cb4c0f4b4b2fa7b98e7bc0cb0a
geir-smukkestad <geir.smukkestad@gmail.com>
2025-10-17 13:49:57 +0200

    Add project files.

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.Coupon> Coupons { get; set; }
+    }
+}
