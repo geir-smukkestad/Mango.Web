ee2c0bea0517302b24ac1d93f75527786e020a11
geir-smukkestad <geir.smukkestad@gmail.com>
2025-10-17 14:38:08 +0200

    Seed database and apply any pending migrations during startup.

--- Mango.Services.CouponAPI/Data/AppDbContext.cs
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.CouponAPI.Data
 {
@@ -8,5 +9,26 @@ namespace Mango.Services.CouponAPI.Data
         {
         }
         public DbSet<Models.Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "10OFF",
+                DiscountAmount = 10.0,
+                MinAmount = 20
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "20OFF",
+                DiscountAmount = 20.0,
+                MinAmount = 40
+            });
+        }
     }
 }
--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -25,5 +25,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigrations();
 app.Run();
+
+
+void ApplyMigrations()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+            _db.Database.Migrate();
+    }
+}
