b00abef0e52d80b653e358d5c0a58d0e99df456c
geir-smukkestad <geir.smukkestad@gmail.com>
2025-10-23 11:59:43 +0200

    Add UI for login and register.

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Common.Dto.Auth;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mango.Web/Service/AuthService.cs
@@ -0,0 +1,55 @@
+﻿using Mango.Common.Dto;
+using Mango.Common.Dto.Auth;
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using static Mango.Web.Utility.StaticDetails;
+
+namespace Mango.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto> AssignRoleAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            var req = new RequestDto
+            {
+                ApiType = ApiType.POST,
+                Data = registrationRequestDto,
+                Url = StaticDetails.AuthAPIBase + "/api/auth/assignRole"
+            };
+
+            return await _baseService.SendAsync(req);
+        }
+
+        public async Task<ResponseDto> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            var req = new RequestDto
+            {
+                ApiType = ApiType.POST,
+                Data = loginRequestDto,
+                Url = StaticDetails.AuthAPIBase + "/api/auth/login"
+            };
+
+            return await _baseService.SendAsync(req);
+        }
+
+        public async Task<ResponseDto> RegisterAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            var req = new RequestDto
+            {
+                ApiType = ApiType.POST,
+                Data = registrationRequestDto,
+                Url = StaticDetails.AuthAPIBase + "/api/auth/register"
+            };
+
+            return await _baseService.SendAsync(req);
+        }
+    }
+}
