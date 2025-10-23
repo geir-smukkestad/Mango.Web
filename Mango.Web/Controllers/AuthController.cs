d56edf2e4e80e2badfcb29cb8720b6f597a9d845
geir-smukkestad <geir.smukkestad@gmail.com>
2025-10-23 13:18:53 +0200

    Implement login and register.

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
-﻿using Mango.Common.Dto.Auth;
+﻿using Mango.Common.Dto;
+using Mango.Common.Dto.Auth;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
@@ -20,10 +24,60 @@ namespace Mango.Web.Controllers
             return View(loginRequestDto);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto responseDto = await _authService.LoginAsync(obj);
+            if (responseDto != null && responseDto.IsSuccessFul)
+            {
+                LoginResponseDto loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", responseDto.Message);
+                return View(obj);
+            }
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{ Text = StaticDetails.RoleAdmin, Value = StaticDetails.RoleAdmin },
+                new SelectListItem{ Text = StaticDetails.RoleCustomer, Value = StaticDetails.RoleCustomer }
+            };
+            ViewBag.RoleList = roleList;
+
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            if (result != null && result.IsSuccessFul)
+            {
+                if (string.IsNullOrEmpty(obj.Role))
+                    obj.Role = StaticDetails.RoleCustomer;
+                ResponseDto assignRoleResult = await _authService.AssignRoleAsync(obj);
+
+                if (assignRoleResult != null && assignRoleResult.IsSuccessFul)
+                {
+                    TempData["success"] = "Registration successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{ Text = StaticDetails.RoleAdmin, Value = StaticDetails.RoleAdmin },
+                new SelectListItem{ Text = StaticDetails.RoleCustomer, Value = StaticDetails.RoleCustomer }
+            };
+            ViewBag.RoleList = roleList;
+
+            return View(obj);
         }
 
         public IActionResult Logout()
