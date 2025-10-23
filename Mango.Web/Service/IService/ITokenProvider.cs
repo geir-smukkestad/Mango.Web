22b4a341e58cc4f91fae7f9924d27518bb4e612f
geir-smukkestad <geir.smukkestad@gmail.com>
2025-10-23 21:52:16 +0200

    Add token cookie. Sign in user via HttpContext.SignInAsync.

---
+++ Mango.Web/Service/IService/ITokenProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ITokenProvider
+    {
+        void SetToken(string token);
+        string? GetToken();
+        void ClearToken();
+    }
+}
